059cd1dca18c9fe9b0088d79f34e1912d47d402e
sbwcartoon <sbwcartoon@gmail.com>
2025-06-26 06:58:03 +0900

    feat: 전체 국가 공휴일 재동기화

---
+++ src/main/java/toy/test/holidaymanager/holiday/application/service/RenewGlobalHolidaysService.java
@@ -0,0 +1,29 @@
+package toy.test.holidaymanager.holiday.application.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import toy.test.holidaymanager.holiday.application.port.in.RenewCountryHolidaysUseCase;
+import toy.test.holidaymanager.holiday.application.port.in.RenewGlobalHolidaysUseCase;
+import toy.test.holidaymanager.holiday.application.port.in.command.RenewCommand;
+import toy.test.holidaymanager.holiday.application.port.out.CountrySourceRepository;
+import toy.test.holidaymanager.holiday.domain.vo.CountryCode;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class RenewGlobalHolidaysService implements RenewGlobalHolidaysUseCase {
+    private final CountrySourceRepository repository;
+    private final RenewCountryHolidaysUseCase renewCountryHolidaysUseCase;
+
+    @Transactional
+    @Override
+    public void execute(final int year) throws JsonProcessingException {
+        List<CountryCode> countryCodes = repository.findAll();
+        for (CountryCode countryCode : countryCodes) {
+            renewCountryHolidaysUseCase.execute(RenewCommand.from(year, countryCode.value()));
+        }
+    }
+}
