a5abec7c26b0f62012b49afc323574c780708aba
sbwcartoon <sbwcartoon@gmail.com>
2025-06-26 17:05:12 +0900

    feat: 공휴일 정렬을 위한 비교 로직 추가

--- src/main/java/toy/test/holidaymanager/holiday/domain/vo/CountryCode.java
+++ src/main/java/toy/test/holidaymanager/holiday/domain/vo/CountryCode.java
@@ -2,10 +2,15 @@ package toy.test.holidaymanager.holiday.domain.vo;
 
 import jakarta.annotation.Nonnull;
 
-public record CountryCode(String value) {
+public record CountryCode(String value) implements Comparable<CountryCode> {
     @Override
     @Nonnull
     public String toString() {
         return value;
+    }
+
+    @Override
+    public int compareTo(CountryCode other) {
+        return this.value.compareTo(other.value);
     }
 }
--- src/main/java/toy/test/holidaymanager/holiday/domain/vo/HolidayLocalName.java
+++ src/main/java/toy/test/holidaymanager/holiday/domain/vo/HolidayLocalName.java
@@ -2,10 +2,15 @@ package toy.test.holidaymanager.holiday.domain.vo;
 
 import jakarta.annotation.Nonnull;
 
-public record HolidayLocalName(String value) {
+public record HolidayLocalName(String value) implements Comparable<HolidayLocalName> {
     @Override
     @Nonnull
     public String toString() {
         return value;
+    }
+
+    @Override
+    public int compareTo(HolidayLocalName other) {
+        return this.value.compareTo(other.value);
     }
 }
